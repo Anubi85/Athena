a1a580c26a1446fab6c4834dadb7ad9a16ebac3f
Andrea <andrea.parisotto.85@gmail.com>
2018-05-27 10:54:04 +0100

    [AP] Fix dependency

--- Athena/ViewModels/ConnectionViewModel.cs
+++ Athena/ViewModels/ConnectionViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Zeus.UI.Mvvm;
+﻿using System.Windows.Input;
+using Zeus.UI.Mvvm;
+using Zeus.UI.Mvvm.Interfaces;
 
 namespace Athena.ViewModels
 {
@@ -13,6 +15,10 @@ namespace Athena.ViewModels
         /// The address of the Athena log server.
         /// </summary>
         private string m_ServerAddress;
+        /// <summary>
+        /// The dialog service that managed the application dialogs.
+        /// </summary>
+        private IDialogService m_DialogService;
 
         #endregion
 
@@ -24,6 +30,8 @@ namespace Athena.ViewModels
         public ConnectionViewModel()
         {
             m_ServerAddress = string.Empty;
+            m_DialogService = ServiceLocator.Resolve<IDialogService>();
+            CloseDialogCommand = new RelayCommand(CloseDialog);
         }
 
         #endregion
@@ -37,6 +45,22 @@ namespace Athena.ViewModels
         {
             get { return m_ServerAddress; }
             set { Set(ref m_ServerAddress, value); }
+        }
+        /// <summary>
+        /// Gets the command that closes the dialog.
+        /// </summary>
+        public ICommand CloseDialogCommand { get; private set; }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Closes the dialog assocated with the view model.
+        /// </summary>
+        private void CloseDialog()
+        {
+            m_DialogService.CloseDialog(this, true);
         }
 
         #endregion
